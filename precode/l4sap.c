9691137257af659f1ed36142bd4e5e88ba05c6a1
vuhger <victor.uhnger@gmail.com>
2025-04-27 14:41:33 +0200

    readd code commments from precode

--- precode/l4sap.c
+++ precode/l4sap.c
@@ -8,6 +8,11 @@
 #include "l4sap.h"
 #include "l2sap.h"
 
+/* Create an L4 client.
+ * It returns a dynamically allocated struct L4SAP that contains the
+ * data of this L4 entity (including the pointer to the L2 entity
+ * used).
+ */
 L4SAP *l4sap_create(const char *server_ip, int server_port)
 {
     if (server_ip == NULL || server_port <= 0)
@@ -43,6 +48,27 @@ L4SAP *l4sap_create(const char *server_ip, int server_port)
     return l4;
 }
 
+/* The functions sends a packet to the network. The packet's payload
+ * is copied from the buffer that it is passed as an argument from
+ * the caller at L5.
+ * If the length of that buffer, which is indicated by len, is larger
+ * than L4Payloadsize, the function truncates the message to L4Payloadsize.
+ *
+ * The function does not return until the correct ACK from the peer entity
+ * has been received.
+ * When a suitable ACK arrives, the function returns the number of bytes
+ * that were accepted for sending (the potentially truncated packet length).
+ *
+ * Waiting for a correct ACK may fail after a timeout of 1 second
+ * (timeval.tv_sec = 1, timeval.tv_usec = 0). The function retransmits
+ * the packet in that case.
+ * The function attempts up to 4 retransmissions. If the last retransmission
+ * fails with a timeout as well, the function returns L4_SEND_FAILED.
+ *
+ * The function may also return:
+ * - L4_QUIT if the peer entity has sent an L4_RESET packet.
+ * - another value < 0 if an error occurred.
+ */
 int l4sap_send(L4SAP *l4, const uint8_t *data, int len)
 {
     if (l4 == NULL || data == NULL || len < 0)
@@ -113,6 +139,17 @@ int l4sap_send(L4SAP *l4, const uint8_t *data, int len)
     return L4_SEND_FAILED;
 }
 
+/* The functions receives a packet from the network. The packet's
+ * payload is copy into the buffer that it is passed as an argument
+ * from the caller at L5.
+ * The function blocks endlessly, meaning that experiencing a timeout
+ * does not terminate this function.
+ * The function returns the number of bytes copied into the buffer
+ * (only the payload of the L4 packet).
+ * The function may also return:
+ * - L4_QUIT if the peer entity has sent an L4_RESET packet.
+ * - another value < 0 if an error occurred.
+ */
 int l4sap_recv(L4SAP *l4, uint8_t *data, int len)
 {
     if (l4 == NULL || data == NULL || len <= 0)
@@ -200,6 +237,12 @@ int l4sap_recv(L4SAP *l4, uint8_t *data, int len)
     return -1;
 }
 
+/** This function is called to terminate the L4 entity and
+ *  free all of its resources.
+ *  We recommend that you send several L4_RESET packets from
+ *  this function to ensure that the peer entity is also
+ *  terminating correctly.
+ */
 void l4sap_destroy(L4SAP *l4)
 {
     if (l4 == NULL)
